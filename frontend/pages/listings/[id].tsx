ec55e6ca1c667514818990a5075cf1f74f7e56f4
Manish Kumar <k88.manish@gmail.com>
2025-12-20 16:36:29 +0700

    chore: remove not found css

--- frontend/pages/listings/[id].tsx
+++ frontend/pages/listings/[id].tsx
@@ -2,7 +2,6 @@
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
 
 export default function ListingDetail() {
   const router = useRouter();
